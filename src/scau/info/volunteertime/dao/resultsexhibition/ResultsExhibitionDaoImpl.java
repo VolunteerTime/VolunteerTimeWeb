e06ce613d47d8c54e54862eb00f68a1e9e73c7f2
chaoKing <786773088@qq.com>
2014-08-16 18:35:36 +0800

    提交初始化内容

---
+++ src/scau/info/volunteertime/dao/resultsexhibition/ResultsExhibitionDaoImpl.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 华南农业大学信息学院蔡超敏2014版权所有
+ * 
+ * 文件创建时间：2014-8-16
+ */
+package scau.info.volunteertime.dao.resultsexhibition;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import scau.info.volunteertime.tool.DatabaseConnection;
+import scau.info.volunteertime.tool.factory.DatabaseConnectionFactory;
+
+/**
+ * @author 蔡超敏
+ * 
+ */
+public class ResultsExhibitionDaoImpl implements ResultsExhibitionDao {
+
+	@Override
+	public String add() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String delete() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String get(int size) {
+		String sql = "SELECT * FROM results ORDER BY id DESC LIMIT 0," + size;
+
+		String jsonInfo = null;
+		Connection conn = null;
+		Statement statement;
+		ResultSet rs;
+		DatabaseConnection dbc = null;
+
+		try {
+			dbc = DatabaseConnectionFactory.getDatabaseConnection();
+			conn = dbc.getConnection();
+			statement = conn.createStatement();
+			rs = statement.executeQuery(sql);
+
+			jsonInfo = resultSetToJsonPagination(rs, size);
+
+			conn.close();
+			dbc.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return jsonInfo;
+	}
+
+	/**
+	 * @param rs
+	 * @param size
+	 * @return String
+	 * @throws JSONException
+	 * @throws SQLException
+	 */
+	private String resultSetToJsonPagination(ResultSet rs, int size)
+			throws SQLException, JSONException {
+		// json数组
+		JSONArray array = new JSONArray();
+		// 获取列数
+		ResultSetMetaData metaData = rs.getMetaData();
+		int columnCount = metaData.getColumnCount();
+
+		// 遍历ResultSet中的每条数据
+		while (rs.next()) {
+			JSONObject jsonObj = new JSONObject();
+
+			// 遍历每一列
+			for (int i = 1; i <= columnCount; i++) {
+				String columnName = metaData.getColumnLabel(i);
+				String value;
+				// if(columnName.contains("PIC")||columnName.contains("pic"))
+				// value =
+				// Environment.getImageServer()+rs.getString(columnName);
+				// else
+				value = rs.getString(columnName);
+				jsonObj.put(columnName, value);
+			}
+			array.put(jsonObj);
+		}
+		JSONObject json = new JSONObject();
+		json.put("records", array);
+
+		json.put("pageSize", size);
+
+		return json.toString();
+	}
+
+}
---
+++ src/scau/info/volunteertime/servlet/resultsexhibition/VolunteerTimeResultExhibitionServlet.java
@@ -0,0 +1,120 @@
+/**
+ * Copyright (c) 华南农业大学信息学院蔡超敏2014版权所有
+ * 
+ * 文件创建时间：2014-8-16
+ */
+package scau.info.volunteertime.servlet.resultsexhibition;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import scau.info.volunteertime.dao.resultsexhibition.ResultsExhibitionDao;
+import scau.info.volunteertime.dao.resultsexhibition.ResultsExhibitionDaoImpl;
+
+/**
+ * Servlet implementation class VolunteerTimeResultExhibitionServlet
+ */
+/**
+ * @author 蔡超敏
+ * 
+ */
+@WebServlet(description = "VolunteerTimeResultExhibitionServlet", urlPatterns = { "/VolunteerTimeResultExhibitionServlet" })
+public class VolunteerTimeResultExhibitionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private static final Log log = LogFactory
+			.getLog(VolunteerTimeResultExhibitionServlet.class);
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public VolunteerTimeResultExhibitionServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String actionType = request.getParameter("action_type");
+		if (actionType != null) {
+			switch (Integer.parseInt(actionType.trim())) {
+			case 0:
+				sendNewData(request, response);
+				break;
+			}
+		}
+	}
+
+	private void sendNewData(HttpServletRequest request,
+			HttpServletResponse response) {
+		int currentPageSize = 0;
+		String currentPageNumberParam = request.getParameter("currentPageSize");
+		if (currentPageNumberParam != null) {
+			currentPageSize = Integer.parseInt(currentPageNumberParam);
+		}
+
+		// 设置输出内容的文档类型和编码
+		response.setContentType("text/javascript;charset=UTF-8");
+
+		log.debug("****************成果展示数据查询****************");
+		log.debug("开始查询······");
+		System.out.println("****************成果展示数据查询****************");
+		System.out.println("开始查询······");
+
+		String rsStr = "";
+
+		// **********************根据当前页数及页面大小从数据库中获取相关信息**********************
+		// 创建Dao对象，用于从数据库查询数据
+
+		ResultsExhibitionDao dao = new ResultsExhibitionDaoImpl();
+
+		try {
+			// 获取系统当前时间，统计 花费时间
+			long startTime = System.currentTimeMillis();
+
+			if (currentPageSize != 0)
+				rsStr = dao.get(currentPageSize);
+			else
+				rsStr = dao.get(5);
+
+			// 获取系统当前时间，统计花费时间
+			long endTime = System.currentTimeMillis();
+			log.debug("查询所花时间：" + (endTime - startTime) + "毫秒");
+			log.debug("查询结果：" + rsStr);
+
+			// 获取http输出流对象
+			PrintWriter writer = response.getWriter();
+			log.debug("开始网络传输······");
+			// 获取系统当前时间，用于统计所花费的时间
+			startTime = System.currentTimeMillis();
+			writer.write(rsStr);
+			// 获取系统当前时间，用于统计所花费的时间
+			endTime = System.currentTimeMillis();
+			log.debug("网络传输所花时间：" + (endTime - startTime) + "毫秒");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
